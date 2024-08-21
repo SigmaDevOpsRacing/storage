caa5b393cf97b3134b2eae92ce636e8a10c8dabf
SigmaDevOpsRacing <inforetal.ar@gmail.com>
2024-08-21 20:48:21 -0300

    agregie el primer ejercicio

---
+++ Ejer01.py
@@ -0,0 +1,12 @@
+def hacer_sandwich():
+    ingredientes = ["pan", "matenca", "jamon", "queso", "lechuga", "tomate"]
+    print("Preparando los ingredienets:", ingredientes)
+    print("tostando el pan.....")
+    print("untando la manteca.....")
+    print("Poniendo feta de jamon.....")
+    print("Poniendo feta de queso.....")
+    print("Poniendo la lechuga.....")
+    print("Poniendo el temate.....")
+    print("Listo para comer.....")
+    return "Buen provecho"
+hacer_sandwich()
